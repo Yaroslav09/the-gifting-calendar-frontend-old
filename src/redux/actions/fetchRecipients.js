76e532a0582aaa9c70c670fb2e06561034b183fd
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-24 20:54:46 -0400

    Create fetch for recipients data

---
+++ src/redux/actions/fetchRecipients.js
@@ -0,0 +1,30 @@
+export function fetchRecipients() {
+  return dispatch => {
+    dispatch(fetchRecipientsBegin());
+    return fetch(`http://localhost:3001/api/recipients`)
+      .then(response => response.json())
+      .then(data => {
+        dispatch(fetchRecipientsSuccess(data));
+        return data;
+      })
+      .catch(error => dispatch(fetchRecipientsFailure(error)));
+  }
+};
+
+export const FETCH_RECIPIENTS_BEGIN = 'FETCH_RECIPIENTS_BEGIN';
+export const FETCH_RECIPIENTS_SUCCESS = 'FETCH_RECIPIENTS_SUCCESS';
+export const FETCH_RECIPIENTS_FAILURE = 'FETCH_RECIPIENTS_FAILURE';
+
+export const fetchRecipientsBegin = () => ({
+  type: FETCH_RECIPIENTS_BEGIN
+});
+
+export const fetchRecipientsSuccess = data => ({
+  type: FETCH_RECIPIENTS_SUCCESS,
+  payload: { data }
+});
+
+export const fetchRecipientsFailure = error => ({
+  type: FETCH_RECIPIENTS_FAILURE,
+  payload: { error }
+});
