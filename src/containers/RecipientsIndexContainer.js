ca48327a214520c042d393feb0a2d381ac1e90e2
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-24 20:55:14 -0400

    Add lifecycle method and rendering

--- src/containers/RecipientsIndexContainer.js
+++ src/containers/RecipientsIndexContainer.js
@@ -1,14 +1,40 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import RecipientsList from '../components/RecipientsList';
+import { fetchRecipients } from '../redux/actions/fetchRecipients';
 
 class RecipientsIndexContainer extends Component {
-  // Fetch all recipients from the backend
-  // Pass data to RecipientsList component
+  componentDidMount() {
+    this.props.fetchRecipients();
+    // fetchRecipients is not executing at this point in the codebase
+    debugger
+  }
 
   render() {
-    return <RecipientsList />
+    const { error, loading, recipients } = this.props;
+
+    if (error) {
+      return <div>Error! {error.message} ...and now the cage of doom squirrels was opened.</div>
+    }
+
+    if (loading) {
+      return <div><iframe src="https://giphy.com/embed/QPQ3xlJhqR1BXl89RG" width="480" height="400" frameBorder="0" className="giphy-embed" alt="Stanley from The Office" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/QPQ3xlJhqR1BXl89RG">via GIPHY</a></p></div>
+    }
+
+    return (
+      // Pass data to RecipientsList component
+      <ul>
+        {recipients.map(recipient =>
+          <li key={recipient.id}>{recipient.name}</li>
+        )}
+      </ul>
+    )
   }
 }
 
-export default connect()(RecipientsIndexContainer);
+const mapStateToProps = state => ({
+  recipients: state.recipients.recipients,
+  loading: state.recipients.loading,
+  error: state.recipients.error
+});
+
+export default connect(mapStateToProps, { fetchRecipients })(RecipientsIndexContainer);
