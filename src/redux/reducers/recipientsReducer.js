8da03648b9c1c0d780c448a54c3317654049b02f
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-24 20:54:21 -0400

    Add fetch action types to recipients reducer

--- src/redux/reducers/recipientsReducer.js
+++ src/redux/reducers/recipientsReducer.js
@@ -1,13 +1,50 @@
-const recipientsReducer = function reducer(state = { recipients: 0 }, action) {
-  console.log('reducer', state, action);
+import {
+  FETCH_RECIPIENTS_BEGIN,
+  FETCH_RECIPIENTS_SUCCESS,
+  FETCH_RECIPIENTS_FAILURE
+} from '../actions/fetchRecipients';
+
+const initialState = {
+  recipients: [],
+  loading: false,
+  error: null
+};
+
+const recipientsReducer = function reducer(
+  state = initialState,
+  action
+) {
   switch (action.type) {
+    case FETCH_RECIPIENTS_BEGIN:
+      return {
+        ...state,
+        loading: true,
+        error: null
+      };
+    case FETCH_RECIPIENTS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        recipients: action.payload.data
+      };
+    case FETCH_RECIPIENTS_FAILURE:
+      return {
+        ...state,
+        loading: false,
+        error: action.payload.error,
+        recipients: []
+      }
     case 'ADD_RECIPIENT':
       return {
-        recipients: state.recipients + 1
+        ...state,
+        recipients: [...state.recipients, action.payload]
       }
     case 'REMOVE_RECIPIENT':
       return {
-        recipients: state.recipients - 1
+        ...state,
+        recipients: state.recipients.filter(
+          recipient => parseInt(recipient.id, 10) !== parseInt(action.payload, 10)
+        )
       }
     // case 'UPDATE_RECIPIENT':
     //   const foundRecipient = state.recipients.find(recipient => parseInt(recipient.id, 10) === parseInt(action.recipient.id, 10));
