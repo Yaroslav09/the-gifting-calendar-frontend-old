07cbbe951d8e020dcce9e5a5fe10f6ea2c6fe742
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-26 18:35:36 -0400

    Clean up container

--- src/containers/RecipientsIndexContainer.js
+++ src/containers/RecipientsIndexContainer.js
@@ -1,34 +1,43 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { fetchRecipients } from '../redux/actions/fetchRecipients';
+import { fetchRecipients } from '../actions/fetchRecipients';
 
 class RecipientsIndexContainer extends Component {
   componentDidMount() {
-    this.props.fetchRecipients();
+    this.props.dispatch(fetchRecipients());
   }
 
   render() {
-    const { loading, recipientIds } = this.props;
-    console.log(recipientIds.recipientIds)
+    const { isLoading, recipients } = this.props;
 
-    if (loading) {
-      return <div><iframe src="https://giphy.com/embed/QPQ3xlJhqR1BXl89RG" width="480" height="400" frameBorder="0" className="giphy-embed" alt="Stanley from The Office" allowFullScreen></iframe><p><a href="https://giphy.com/gifs/QPQ3xlJhqR1BXl89RG">via GIPHY</a></p></div>
+    if (isLoading) {
+      return (
+        <div>
+          <iframe
+            src="https://giphy.com/embed/QPQ3xlJhqR1BXl89RG"
+            width="480"
+            height="400"
+            frameBorder="0"
+            className="giphy-embed"
+            alt="Stanley from The Office"
+            allowFullScreen
+          ></iframe>
+          <p>
+            <a href="https://giphy.com/gifs/QPQ3xlJhqR1BXl89RG">via GIPHY</a>
+          </p>
+        </div>
+      )
     }
 
     return (
       // Pass data to RecipientsList component
       <ul>
-        {recipientIds.map(recipientId =>
-          <li key={recipientId}>{recipientId}</li>
+        {recipients.map(({ id, name }) =>
+          <li key={id}>Name: {name}</li>
         )}
       </ul>
     )
   }
 }
 
-const mapStateToProps = state => ({
-  recipientIds: state.recipient.recipientIds,
-  loading: state.recipient.loading
-});
-
-export default connect(mapStateToProps, { fetchRecipients })(RecipientsIndexContainer);
+export default connect()(RecipientsIndexContainer);
